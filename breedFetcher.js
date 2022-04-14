dfd92f66335aa0673071bd7abd247aaa07c20854
MitchScho <scho.mitch@gmail.com>
2022-04-14 23:49:25 +0000

    lint breedFetcher file

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,13 +1,8 @@
 const request = require("request");
 
-
-// const fs = require("fs");
 const breed = process.argv[2];
 const url = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`;
-//const filePath = process.argv[3];
 
-// 2 async operations
-//Make an HTTP request and wait for response
 request(url, (error, response, body) => {
   //In the event that the request fails, the error argument in our callback function would NOT be undefined, and instead contain details.
   
@@ -24,8 +19,5 @@ request(url, (error, response, body) => {
     return;
   }
   console.log("description", data[0].description);
-  
-
-
   
 });
