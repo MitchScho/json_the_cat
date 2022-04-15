dcb546e0d4ef044319b4f2d745a53365559e2c6c
MitchScho <scho.mitch@gmail.com>
2022-04-15 02:18:49 +0000

    BreedFetcher refactoring

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,23 +1,29 @@
 const request = require("request");
 
-const breed = process.argv[2];
-const url = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`;
 
-request(url, (error, response, body) => {
-  //In the event that the request fails, the error argument in our callback function would NOT be undefined, and instead contain details.
-  
-  if (error) {
-    console.log("ERROR:", error);
-    return;
-  }
+const fetchBreedDescription = function(breedName, callback) {
 
-  const data = JSON.parse(body);
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
   
-  if (!data[0]) {
-    //Write code to output an appropriate message if the requested breed is not found
-    console.log("ERROR: Breed not found");
-    return;
-  }
-  console.log("description", data[0].description);
-  
-});
+  request(url, (error, response, body) => {
+    //In the event that the request fails, the error argument in our callback function would NOT be undefined, and instead contain details.
+    
+    if (error) {
+      
+      return callback(error, null);
+    }
+    
+    const data = JSON.parse(body);
+
+    if (data[0]) {
+      //Write code to output an appropriate message if the requested breed is not found
+      
+      callback(null, data[0].description);
+    } else {
+      callback("ERROR: Breed does not exist");
+    }
+    
+    
+  });
+};
+module.exports = { fetchBreedDescription };
